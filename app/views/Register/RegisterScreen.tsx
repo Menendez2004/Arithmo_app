2f19a16bf3854e015b5634e8cfdd3526a1e38431
Menendez2004 <nitrotruelo@gmail.com>
2023-04-25 23:37:07 -0600

    no se

--- app/views/Register/RegisterScreen.tsx
+++ app/views/Register/RegisterScreen.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Text, View, Image, TextInput, ToastAndroid, TouchableOpacity } from 'react-native';
-import RegisterStyles from '../../styles/RegisterStyle';
-import { RoundedBtm } from '../../RoundedBtm';
-import { RestructuringImput } from '../../RestructuringImput';
-import { RootStackParamList } from '../../../App';
-import usesViewModelRegister from './ServiceRegister';
+import React from "react";
+import {Text,View,Image,TextInput,ToastAndroid,TouchableOpacity} from "react-native";
+import RegisterStyles from "../../styles/RegisterStyle";
+import { RoundedBtm } from "../../RoundedBtm";
+import { RestructuringImput } from "../../RestructuringImput";
+import { RootStackParamList } from "../../../App";
+import usesViewModelRegister from "./ServiceRegister";
 
 export const RegisterScreen = () => {
-
-    const { name, email, lastName, password, confirmPassword, onChange, Register } = usesViewModelRegister()
-
+    const {
+        name,
+        email,
+        lastName,
+        password,
+        confirmPassword,
+        onChange,
+        Register,
+    } = usesViewModelRegister();
 
     return (
         <View style={RegisterStyles.container}>
@@ -23,82 +29,81 @@ export const RegisterScreen = () => {
 
                 <Image
                     style={RegisterStyles.burbu}
-                    source={require('../../imgs/burbu.png')}
+                    source={require("../../imgs/burbu.png")}
                 />
 
                 <View style={RegisterStyles.logoContainer}>
                     <Image
-                        source={require('../../imgs/user_image.png')}
+                        source={require("../../imgs/user_image.png")}
                         style={RegisterStyles.logoImage}
                     />
                     <Text style={RegisterStyles.logotext}>Seleciona una imagen</Text>
                 </View>
 
-                <RestructuringImput
-                    placeholder='Nombres'
-                    KeyboardType='default'
-                    image={require('../../imgs/Name.png')}
-                    property='name'
-                    onChangeText={onChange}
-                    value={name}
-                />
-
-
-
-                <RestructuringImput
-                    placeholder='Apellidos'
-                    KeyboardType='default'
-                    image={require('../../imgs/lastname.png')}
-                    property='lastName'
-                    onChangeText={onChange}
-                    value={lastName}
-                />
-
-
-
-                <RestructuringImput
-                    placeholder='Email'
-                    KeyboardType='default'
-                    image={require('../../imgs/email.png')}
-                    property='email'
-                    onChangeText={onChange}
-                    value={email}
-                />
-
-
-                <RestructuringImput
-                    placeholder='Contraseña'
-                    KeyboardType='default'
-                    image={require('../../imgs/Password.png')}
-                    property='password'
-                    onChangeText={onChange}
-                    value={password}
-                    secureTextEntry={true}
-                />
-
-                <RestructuringImput
-                    placeholder='Confirmar Contraseña'
-                    KeyboardType='default'
-                    image={require('../../imgs/confirmpass.png')}
-                    property='confirmPassword'
-                    onChangeText={onChange}
-                    value={confirmPassword}
-                    secureTextEntry={true}
-                />
-
-                <View >
-                    <RoundedBtm
-                        text='CONFIRMAR'
-                        onPress={() => Register()}
+                    <RestructuringImput
+                        placeholder="Nombres"
+                        KeyboardType="default"
+                        image={require("../../imgs/Name.png")}
+                        property="name"
+                        onChangeText={onChange}
+                        value={name}
                     />
-                </View>
+
+
+
+                    <RestructuringImput
+                        placeholder="Apellidos"
+                        KeyboardType="default"
+                        image={require("../../imgs/lastname.png")}
+                        property="lastName"
+                        onChangeText={onChange}
+                        value={lastName}
+                    />
+
+
+
+                    <RestructuringImput
+                        placeholder="Email"
+                        KeyboardType="default"
+                        image={require("../../imgs/email.png")}
+                        property="email"
+                        onChangeText={onChange}
+                        value={email}
+                    />
+
+
+                    <RestructuringImput
+                        placeholder="Contraseña"
+                        KeyboardType="default"
+                        image={require("../../imgs/Password.png")}
+                        property="password"
+                        onChangeText={onChange}
+                        value={password}
+                        secureTextEntry={true}
+                    />
+
+                    <RestructuringImput
+                        placeholder="Confirmar Contraseña"
+                        KeyboardType="default"
+                        image={require("../../imgs/confirmpass.png")}
+                        property="confirmPassword"
+                        onChangeText={onChange}
+                        value={confirmPassword}
+                        secureTextEntry={true}
+                    />
+
+                    <View>
+                        <RoundedBtm text="CONFIRMAR" onPress={() => Register()} />
+                    </View>
+
 
                 <Image
                     style={RegisterStyles.rightBurble}
-                    source={require('../../imgs/burbuf.png')}
+                    source={require("../../imgs/burbuf.png")}
                 />
+
             </View>
 
-        </View >
-    )
-}
+        </View>
+    );
+};
